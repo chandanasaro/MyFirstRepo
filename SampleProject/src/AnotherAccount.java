b67a8b6f695c7cdbd41b82ae71e8ea835073f749
Mohana <Mohana@192.168.1.38>
2021-12-01 16:16:54 +0530

    modified acc java

--- SampleProject/src/AnotherAccount.java
+++ SampleProject/src/AnotherAccount.java
@@ -3,6 +3,7 @@ public class AnotherAccount {
 
 	public static void main(String[] args) {
 		System.out.println("another acc created");
+		System.out.println("added more code in the class");
 
 	}
 
