d69c0a9dd5f7f47e7365e89602ad249a0edcc2fc
saroc <saroc@LAPTOP-US03I71U>
2024-12-30 14:35:26 +0530

    Added the variables

--- SampleProject/src/AnotherAccount.java
+++ SampleProject/src/AnotherAccount.java
@@ -4,6 +4,9 @@ public class AnotherAccount {
 	public static void main(String[] args) {
 		System.out.println("another acc created");
 		System.out.println("added more code in the class");
+int a=10;
+int b=20;
+
 
 	}
 
